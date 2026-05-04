01178c6638bc11202a26941655b5a2e2022de824
OrSasson1407 <orsasson1407@gmail.com>
2026-05-04 12:51:10 +0300

    Complete TCP client-server implementation with echo loop

--- 4_tcp_c.cpp
+++ 4_tcp_c.cpp
@@ -1,67 +1,82 @@
 #include <iostream>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#include <stdio.h>
-#include <string.h>
 
-#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "ws2_32.lib") // Link Winsock library
 
 using namespace std;
 
 int main() {
-    // 1. Initialize Winsock
+    // -----------------------------
+    // Initialize Winsock
+    // -----------------------------
     WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        cout << "WSAStartup failed." << endl;
+    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0) {
+        cerr << "WSAStartup failed.\n";
         return 1;
     }
 
-    const char* ip_address = "127.0.0.1";
-    const int port_no = 8080;
+    const char* server_ip = "127.0.0.1"; // Server IP (localhost)
+    int server_port = 5555;              // Server port
 
+    // -----------------------------
+    // Create TCP socket
+    // -----------------------------
     SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == INVALID_SOCKET) {
-        cout << "error creating socket: " << WSAGetLastError() << endl;
+        cerr << "Error creating socket: " << WSAGetLastError() << endl;
         WSACleanup();
         return 1;
     }
 
-    struct sockaddr_in sin;
-    memset(&sin, 0, sizeof(sin));
-    sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = inet_addr(ip_address);
-    sin.sin_port = htons(port_no);
+    // -----------------------------
+    // Setup server address
+    // -----------------------------
+    sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(server_port);          // Port in network byte order
+    server_addr.sin_addr.s_addr = inet_addr(server_ip); // Convert string IP to numeric
 
-    if (connect(sock, (struct sockaddr *) &sin, sizeof(sin)) == SOCKET_ERROR) {
-        cout << "error connecting to server: " << WSAGetLastError() << endl;
-    }
-
-    char data_addr[] = "Im a message";
-    int data_len = strlen(data_addr);
-    
-    int sent_bytes = send(sock, data_addr, data_len, 0);
-    if (sent_bytes == SOCKET_ERROR) {
-        cout << "error writing to socket" << endl;
-    }
-
-    char buffer[4096];
-    int expected_data_len = sizeof(buffer);
-    
-    int read_bytes = recv(sock, buffer, expected_data_len, 0);
-    if (read_bytes == 0) {
-        cout << "Connection closed by server." << endl;
-    }
-    else if (read_bytes == SOCKET_ERROR) {
-        cout << "error reading from socket: " << WSAGetLastError() << endl;
-    }
-    else {
-        buffer[read_bytes] = '\0';
-        cout << buffer << endl;
+    // -----------------------------
+    // Connect to server
+    // -----------------------------
+    if (connect(sock, (sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+        cerr << "Connect failed: " << WSAGetLastError() << endl;
+        closesocket(sock);
+        WSACleanup();
+        return 1;
     }
 
-    // 2. Cleanup
-    closesocket(sock);
-    WSACleanup();
+    cout << "Connected to server!\n";
+
+    // -----------------------------
+    // Message sending loop
+    // -----------------------------
+    string msg;
+    cout << "Message to send: ";
+    getline(cin, msg);
+
+    while (msg != "quit") {
+        // Send message to server
+        send(sock, msg.c_str(), msg.length(), 0);
+
+        // Receive echo from server
+        char buffer[4096];
+        int bytes_received = recv(sock, buffer, sizeof(buffer)-1, 0);
+        if (bytes_received > 0) {
+            buffer[bytes_received] = '\0'; // Null-terminate
+            cout << "Server replied: " << buffer << endl;
+        }
+
+        cout << "Message to send: ";
+        getline(cin, msg);
+    }
+
+    // -----------------------------
+    // Clean up
+    // -----------------------------
+    closesocket(sock); // Close client socket
+    WSACleanup();      // Cleanup Winsock
 
     return 0;
 }
--- 4_tcp_s.cpp
+++ 4_tcp_s.cpp
@@ -1,76 +1,94 @@
 #include <iostream>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#include <stdio.h>
-#include <string.h>
 
-#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "ws2_32.lib") // Link Winsock library
 
 using namespace std;
 
 int main() {
-    // 1. Initialize Winsock
+    // -----------------------------
+    // Initialize Winsock
+    // -----------------------------
     WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        cout << "WSAStartup failed." << endl;
+    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0) {
+        cerr << "WSAStartup failed.\n";
         return 1;
     }
 
-    const int server_port = 8080;
+    int server_port = 5555; // Server port
 
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == INVALID_SOCKET) {
-        cout << "error creating socket: " << WSAGetLastError() << endl;
+    // -----------------------------
+    // Create TCP socket
+    // -----------------------------
+    SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_sock == INVALID_SOCKET) {
+        cerr << "Error creating socket: " << WSAGetLastError() << endl;
         WSACleanup();
         return 1;
     }
 
-    struct sockaddr_in sin;
-    memset(&sin, 0, sizeof(sin));
-    sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = INADDR_ANY;
-    sin.sin_port = htons(server_port);
+    // -----------------------------
+    // Bind socket to all interfaces
+    // -----------------------------
+    sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;          // IPv4
+    server_addr.sin_addr.s_addr = INADDR_ANY;  // Listen on all network interfaces
+    server_addr.sin_port = htons(server_port); // Port in network byte order
 
-    if (bind(sock, (struct sockaddr *) &sin, sizeof(sin)) == SOCKET_ERROR) {
-        cout << "error binding socket: " << WSAGetLastError() << endl;
+    if (bind(listen_sock, (sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+        cerr << "Bind failed: " << WSAGetLastError() << endl;
+        closesocket(listen_sock);
+        WSACleanup();
+        return 1;
     }
 
-    if (listen(sock, 5) == SOCKET_ERROR) {
-        cout << "error listening to a socket: " << WSAGetLastError() << endl;
+    // -----------------------------
+    // Listen for incoming connections
+    // -----------------------------
+    if (listen(listen_sock, 5) == SOCKET_ERROR) {
+        cerr << "Listen failed: " << WSAGetLastError() << endl;
+        closesocket(listen_sock);
+        WSACleanup();
+        return 1;
     }
 
-    struct sockaddr_in client_sin;
-    int addr_len = sizeof(client_sin);
-    
-    SOCKET client_sock = accept(sock,  (struct sockaddr *) &client_sin,  &addr_len);
+    cout << "Server listening on port " << server_port << "...\n";
+
+    // -----------------------------
+    // Accept a client connection
+    // -----------------------------
+    sockaddr_in client_addr;
+    int client_addr_size = sizeof(client_addr);
+    SOCKET client_sock = accept(listen_sock, (sockaddr*)&client_addr, &client_addr_size);
     if (client_sock == INVALID_SOCKET) {
-        cout << "error accepting client: " << WSAGetLastError() << endl;
+        cerr << "Accept failed: " << WSAGetLastError() << endl;
+        closesocket(listen_sock);
+        WSACleanup();
+        return 1;
     }
 
+    cout << "Client connected!\n";
+
+    // -----------------------------
+    // Receive data from client
+    // -----------------------------
     char buffer[4096];
-    int expected_data_len = sizeof(buffer);
-    
-    int read_bytes = recv(client_sock, buffer, expected_data_len, 0);
-    if (read_bytes == 0) {
-        cout << "connection closed by client" << endl;
-    }
-    else if (read_bytes == SOCKET_ERROR) {
-        cout << "error reading from socket" << endl;
-    }
-    else {
-        buffer[read_bytes] = '\0';
-        cout << buffer << endl;
+    int bytes_received = recv(client_sock, buffer, sizeof(buffer)-1, 0); // -1 to leave space for null terminator
+    if (bytes_received > 0) {
+        buffer[bytes_received] = '\0'; // Null-terminate the received data
+        cout << "Received: " << buffer << endl;
+
+        // Echo the same message back to the client
+        send(client_sock, buffer, bytes_received, 0);
     }
 
-    int sent_bytes = send(client_sock, buffer, read_bytes, 0);
-    if (sent_bytes == SOCKET_ERROR) {
-        cout << "error sending to client" << endl;
-    }
-
-    // 2. Cleanup
-    closesocket(client_sock);
-    closesocket(sock);
-    WSACleanup();
+    // -----------------------------
+    // Clean up
+    // -----------------------------
+    closesocket(client_sock); // Close client socket
+    closesocket(listen_sock); // Close listening socket
+    WSACleanup();             // Cleanup Winsock
 
     return 0;
 }
